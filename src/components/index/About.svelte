abcff75f7a544a7473326749b6f15591e4057678
Eunseop Shim <lumeriith@gmail.com>
2022-03-12 01:25:31 +0900

    Change About part layout

--- src/components/index/About.svelte
+++ src/components/index/About.svelte
@@ -4,13 +4,13 @@
 	import FullWidthSpace from '@components/general/FullWidthSpace.svelte';
 </script>
 
-<FullWidthSpace class="pt-3 pb-5 text-center">
+<FullWidthSpace class="text-center">
 	<Container>
-		<h2 class="font-bold text-lg opacity-90">Contact</h2>
-		<a class="text-xs opacity-50 mb-6 inline-block" href="mailto:lumeriith@gmail.com"
+		<h2 class="font-bold text-lg opacity-90 mb-1">Contact</h2>
+		<a class="text-sm opacity-50 mb-6 inline-block" href="mailto:lumeriith@gmail.com"
 			>lumeriith@gmail.com</a
 		>
-		<div class="flex justify-center items-center gap-3 mb-4">
+		<div class="flex justify-center items-center gap-3 mb-14">
 			<IconButton class="white-icon-button" href="https://github.com/lumeriith" target="_blank">
 				<img
 					src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
@@ -29,9 +29,9 @@
 			</IconButton>
 		</div>
 		<div class="flex flex-row gap-2 justify-center text-xs opacity-50">
-			<div>Made with ❤️ in South Korea</div>
+			<div>Made with ❤️ by Eunseop Shim</div>
 			<div>·</div>
-			<div>Powered by Svelte</div>
+			<div>Powered by SvelteKit</div>
 		</div>
 	</Container>
 </FullWidthSpace>
