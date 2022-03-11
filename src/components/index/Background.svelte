1fac87281d1d0ed41d93ab8042b603861549caa0
Eunseop Shim <lumeriith@gmail.com>
2022-03-12 02:22:32 +0900

    Make index background more consistent

--- src/components/index/Background.svelte
+++ src/components/index/Background.svelte
@@ -4,17 +4,7 @@
 	div {
 		position: fixed;
 		inset: 0;
-		background: url(/gradientbg.png) no-repeat center center fixed;
-		-webkit-background-size: cover;
-		-moz-background-size: cover;
-		-o-background-size: cover;
-		background-size: cover;
-	}
-
-	@supports (-webkit-touch-callout: none) {
-		div {
-			background: url(/gradientbg.png) no-repeat center top scroll;
-			background-size: auto 100vh;
-		}
+		background: url(/gradientbg.png) no-repeat;
+		background-size: 100% 100%;
 	}
 </style>
