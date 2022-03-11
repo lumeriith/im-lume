a0be1e55579c74438a6a675d141587d07f9ebc49
Eunseop Shim <lumeriith@gmail.com>
2022-03-12 01:24:22 +0900

    Add Svelte to experiences

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -12,7 +12,7 @@ export default [
 	{
 		title: 'JavaScript',
 		subtitle: '6개월',
-		details: ['Node.js', 'React.js', 'Svelte', 'Redux'],
+		details: ['Node.js', 'React.js', 'Redux'],
 		imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
 		background: 'hsl(56, 100%, 95%)',
 		color: 'hsl(56, 100%, 10%)'
@@ -41,5 +41,13 @@ export default [
 		imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
 		background: 'hsl(207, 51%, 20%)',
 		color: 'white'
+	},
+	{
+		title: 'Svelte',
+		subtitle: '2개월',
+		details: [['SvelteKit', 'https://svelte.dev/']],
+		imgUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg',
+		background: 'white',
+		color: 'hsl(12, 100%, 50%)'
 	}
 ];
