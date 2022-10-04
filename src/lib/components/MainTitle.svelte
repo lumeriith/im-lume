63cd0d29cafcb6c152cfa41645f18164398de32a
Eunseop Shim <lumeriith@gmail.com>
2022-10-04 19:14:54 +0900

    Fullscreen hero page

--- src/lib/components/MainTitle.svelte
+++ src/lib/components/MainTitle.svelte
@@ -15,42 +15,35 @@
 </script>
 
 <div
-	class="h-64 pt-6"
+	class="h-[100vh] pt-6 flex justify-center items-center font-monospace"
 	style="background: url({bg}); background-repeat: no-repeat; background-size: 100% 100%"
 >
-	<Container class="font-monospace">
+	<div class="pb-12">
 		<header class="flex gap-2 items-center mb-4">
-			<div class="flex text-3xl">
-				<Icon icon="fluent-emoji-high-contrast:lizard" />
-				<Icon icon="fluent:drink-to-go-20-filled" />
+			<div class="flex items-center">
+				<Icon icon="fluent-emoji-high-contrast:lizard" class="text-[27px] text-[#87ff1e]" />
+				<Icon icon="fluent:drink-to-go-20-filled" class="text-[32px] text-[#ff9db2]" />
 			</div>
 			<div class="title text-2xl text-center typewriter px-1 whitespace-nowrap typewriter">
 				{typewriterText.substring(0, currentTypewriterLength)}
 			</div>
 		</header>
 
-		<section class="text-md">
+		<section class="text-md content">
 			<div>- Kyunghee University Undergraduate</div>
 			<div>- Wannabe Game Developer</div>
 			<div>- Hobbiest Web Developer</div>
 		</section>
 		<div class="h-4" />
 		<main class="text-md">Personal Portfolio</main>
-	</Container>
+	</div>
 </div>
 
 <style>
-	.title {
-		filter: brightness(1.2) saturate(1.2);
+	.content {
+		filter: brightness(1.8);
 		text-shadow: 0 0 20px rgba(255, 81, 148, 0.4);
 		background: linear-gradient(135deg, #ff71b8 0%, #ff2982 40%, #a47dff 70%, #6c82ff 100%);
-		background-clip: text;
-		-webkit-background-clip: text;
-		-webkit-text-fill-color: transparent;
-	}
-
-	.subtitle {
-		background: linear-gradient(135deg, #ff69b4 0%, #f00064 40%, #c64aff 70%, #6b4aff 100%);
 		background-clip: text;
 		-webkit-background-clip: text;
 		-webkit-text-fill-color: transparent;
