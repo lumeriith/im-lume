266c5d0cff7f1483d38bd284c214ffd5f16c7484
Eunseop Shim <lumeriith@gmail.com>
2022-10-07 13:02:05 +0900

    Fix navigation scrolling issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,24 @@
 <script>
-	import LizardSmoothie from '$lib/components/LizardSmoothie.svelte';
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
 	import 'ress/dist/ress.min.css';
+	import { onMount } from 'svelte';
 	import '../app.css';
+
+	let root;
+
+	onMount(() => {
+		root = document.getElementsByTagName('html')[0];
+
+		root?.classList.add('smoothscroll');
+	});
+
+	beforeNavigate(() => {
+		root?.classList.remove('smoothscroll');
+	});
+
+	afterNavigate(() => {
+		root?.classList.add('smoothscroll');
+	});
 </script>
 
 <slot />
